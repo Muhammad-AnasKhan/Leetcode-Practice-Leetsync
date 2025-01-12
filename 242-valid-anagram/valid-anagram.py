a686725e0104ce88ddfbf77f2783898ea25044ef
Muhammad Ans Khan <91449185+Muhammad-AnasKhan@users.noreply.github.com>
2025-01-12 08:56:46 +0500

    Time: 3 ms (98.68%) | Memory: 17.8 MB (46.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,19 +3,28 @@ class Solution:
         if len(s) != len(t):
             return False
         # using sorting
-        # return sorted(s) == sorted(t)
+            # return sorted(s) == sorted(t)
 
         # using hashset(tracking count for better efficiency as compared to sorting)
+
         #  We can use 2 hashsets to keep count and then compare them
         #  We can also use one hashset to ++ and -- the count of each character
+        
+        # counter = {}
+        # for i in s:
+        #     counter[i] = counter.get(i,0)+1
+        # for j in t:
+        #     if j not in counter or counter[j] == 0:
+        #         return False
+        #     counter[j] -=1
 
-        counter = {}
-        for i in s:
-            counter[i] = counter.get(i,0)+1
-        for j in t:
-            if j not in counter or counter[j] == 0:
+        # return True
+
+        # using builtin count method
+        for idx in set(s):
+            # Compare s.count(l) and t.count(l) for every index i from 0 to 26...
+            # If they are different, return false...
+            if s.count(idx) != t.count(idx):
                 return False
-            counter[j] -=1
-
-        return True
+        return True     # Otherwise, return true...
         

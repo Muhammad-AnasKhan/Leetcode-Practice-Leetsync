04ad5f865d24871f5d85e49ba42572554477ae72
Muhammad Ans Khan <91449185+Muhammad-AnasKhan@users.noreply.github.com>
2025-01-26 10:49:26 +0500

    Time: 1 ms (95.42%) | Memory: 20.7 MB (77.14%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,17 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:        
+        #  the idea is to compare the ending and starting value of nested arrays and update the last index of nested array
+
         merged = []
         intervals.sort(key=lambda x: x[0])
-
         prev = intervals[0]
-
         for interval in intervals[1:]:
-            if interval[0] <= prev[1]:
-                prev[1] = max(prev[1], interval[1])
+            if(interval[0]<=prev[1]):
+                prev[1] = max(prev[1],interval[1])
             else:
                 merged.append(prev)
                 prev = interval
-        
         merged.append(prev)
-
         return merged
+

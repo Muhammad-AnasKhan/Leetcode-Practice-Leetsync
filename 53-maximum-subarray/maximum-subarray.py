33ee758d2a4524152c67f727f3ace92258d41d87
Muhammad Ans Khan <91449185+Muhammad-AnasKhan@users.noreply.github.com>
2025-01-26 10:01:47 +0500

    Time: 49 ms (79.03%) | Memory: 32.9 MB (17.12%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,13 +1,22 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:            
-        res = nums[0]
-        total = 0
-
-        for n in nums:
-            if total < 0:
-                total = 0
-
-            total += n
-            res = max(res, total)
+        # Cubic solution:
+        # 1 loop for i, one for j (sub array), 1 for k(to calculate sub array sum)
         
-        return res
+        # Quadratic solution:
+        # 1 loop for i, i for j (subarray), and save the previous subarray sum 
+
+        # linear solution
+        # the idea is to go through the array, and ignore the the sub arrays resulting in -ve sum(value)
+
+        maxSubSum = nums[0]
+        currentSum = 0
+
+        for i in nums:
+            if currentSum < 0:
+                currentSum = 0 # reset to 0 if found -ve result, to keep the result contiguos
+
+            currentSum += i
+            maxSubSum = max(currentSum, maxSubSum)
+
+        return maxSubSum

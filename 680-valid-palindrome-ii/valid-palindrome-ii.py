449df32b8693926a3fd7e4d14d9bd4476ea386fe
Muhammad Ans Khan <91449185+Muhammad-AnasKhan@users.noreply.github.com>
2025-07-13 18:44:50 +0500

    Time: 66 ms (36.44%) | Memory: 17.9 MB (74.06%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         l,r = 0, len(s) -1
         while(l<r):
-            if s[l] != s[r]:
+            if s[l] != s[r]:#if we encounter unequal corresponding letters, try skipping any
                 return self.isValidSubPalindrome(s, l + 1, r) or self.isValidSubPalindrome(s, l, r-1)
             
             l, r = l + 1, r -1

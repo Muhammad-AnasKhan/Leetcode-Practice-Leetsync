eeec626a958956b63bfd897e4aba1764b8b7f383
Muhammad Ans Khan <91449185+Muhammad-AnasKhan@users.noreply.github.com>
2025-07-13 12:46:55 +0500

    Time: 119 ms (34.46%) | Memory: 26.9 MB (32.56%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        buy = 0
+        sell = 1
+        while (buy < sell and sell < len(prices)):
+            if (prices[buy] < prices[sell] and buy < sell):
+                max_profit = max(max_profit, prices[sell] - prices[buy])
+            else:
+                buy = sell
+            sell = sell+1
+        return max_profit
+        
         # Brute Force
         # max_profit = 0
         # for i in range(len(prices)):
@@ -10,14 +21,15 @@ class Solution:
         # return max_profit
 
         # Using One Pass
-        min_price = prices[0]
-        max_profit = 0
+        # calculate the min, max value and get the max profit
+        # min_price = prices[0]
+        # max_profit = 0
 
-        for i in range(1, len(prices)):
-            # min_price = min(min_price, prices[i])
-            # max_profit = max(max_profit, prices[i] - min_price)
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > max_profit:
-                max_profit = prices[i] - min_price
-        return max_profit
+        # for i in range(1, len(prices)):
+        #     # min_price = min(min_price, prices[i])
+        #     # max_profit = max(max_profit, prices[i] - min_price)
+        #     if prices[i] < min_price:
+        #         min_price = prices[i]
+        #     elif prices[i] - min_price > max_profit:
+        #         max_profit = prices[i] - min_price
+        # return max_profit

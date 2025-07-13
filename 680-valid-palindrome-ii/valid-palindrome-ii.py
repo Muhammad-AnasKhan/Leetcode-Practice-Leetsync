1db460392f8c73bdcfb14a51763a66350f2e7fc8
Muhammad Ans Khan <91449185+Muhammad-AnasKhan@users.noreply.github.com>
2025-07-13 18:44:02 +0500

    Time: 60 ms (64.59%) | Memory: 17.8 MB (92.11%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l,r = 0, len(s) -1
+        while(l<r):
+            if s[l] != s[r]:
+                return self.isValidSubPalindrome(s, l + 1, r) or self.isValidSubPalindrome(s, l, r-1)
+            
+            l, r = l + 1, r -1
+        return True
+    # helper function to calcaulte sub palindrome
+    def isValidSubPalindrome(self,s, l,r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r -1
+        return True
